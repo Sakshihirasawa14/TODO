a470f94b603d2fa61d9cb68d2031d7c3578b273c
Sakshi Koundal <47637459+Sakshihirasawa14@users.noreply.github.com>
2021-02-28 17:37:30 +0530

    update_1

--- TODO.js
+++ TODO.js
@@ -1,40 +1,69 @@
- var btnAdd = document.getElementById("badd");
- var list = document.getElementById("todo-list");
- var inputBox = document.getElementById("todo-input");
+//JS code for TODO 
 
- var currentInput = "";
- inputBox.addEventListener("input", function(e) {
-     currentInput = e.target.value;
- });
-
-
- inputBox.addEventListener("keyup", function(e) {
-     if (e.keyCode === 13) {
-         addListItems();
-     }
- });
+var btnAdd = document.getElementById("badd");
+// var btnUpdate = document.getElementById("bupdate");
+var btnRemove = document.getElementById("bremove");
+var list = document.getElementById("todo-list");
+var inputBox = document.getElementById("todo-input");
 
 
- function addListItems() {
-
-     if (currentInput !== undefined && currentInput !== null && currentInput !== "") {
-
-         var newList = document.createElement("li");
-         var textNode = document.createTextNode(currentInput);
-         newList.appendChild(textNode);
-         newList.id = "item" + (list.childElementCount + 1);
-
-         list.appendChild(newList);
-         inputBox.value = "";
-         currentInput = "";
-     } else {
-         alert("Please, Enter valid TO-DO item!!");
-     }
+var currentInput = "";
+inputBox.addEventListener("input", function(e) {
+    currentInput = e.target.value;
+});
 
 
- }
+inputBox.addEventListener("keyup", function(e) {
+    if (e.keyCode === 13) {
+        addListItems();
+    }
+});
+
+
+function createNewNode() {
+    var newList = document.createElement("li");
+    var textNode = document.createTextNode(currentInput);
+    newList.appendChild(textNode);
+    newList.id = "item" + (list.childElementCount + 1);
+
+    return newList;
+};
+
+
+function addListItems() {
+
+    if (currentInput !== undefined && currentInput !== null && currentInput !== "") {
+        var newList = createNewNode();
+
+        list.appendChild(newList);
+        inputBox.value = "";
+        currentInput = "";
+    } else {
+        alert("Please, Enter valid TO-DO item!!");
+    }
+
+
+};
 
 
 
 
- btnAdd.addEventListener('click', addListItems);
+btnAdd.addEventListener('click', addListItems);
+
+
+
+// btnUpdate.addEventListener('click', function() {
+//     var firstElement = list.firstElementChild;
+//     var newList = createNewNode;
+
+
+//     list.removeChild(newList, firstElement);
+// });
+
+btnRemove.addEventListener('click', function() {
+    var firstElement = list.firstElementChild;
+
+    list.removeChild(firstElement);
+
+
+});
